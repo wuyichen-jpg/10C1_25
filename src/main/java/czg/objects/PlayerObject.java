f2d7b9af532657fe8d53e4e9956684d779916f3b
helmo2019 <helmo2019@yahoo.com>
2026-03-09 09:56:15 +0100

    PlayerObject aufräumen

--- src/main/java/czg/objects/PlayerObject.java
+++ src/main/java/czg/objects/PlayerObject.java
@@ -1,5 +1,8 @@
 package czg.objects;
 
+import czg.scenes.BaseScene;
+import czg.scenes.InventarScene;
+import czg.scenes.SceneStack;
 import czg.util.Images;
 
 import java.awt.*;
@@ -12,30 +15,21 @@ import java.util.List;
  */
 public class PlayerObject extends BaseObject{
 
-    //Festlegen der benötigten Variablen (Lebenspunkte, veränderliche Charaktereigentschaften)
-    public String name;
     //Anlegen einer Reihung "Inventar", in welchem die Items, auf die der Spieler zugreifen kann, gespeichert werden
     public List<ItemObject> inventar;
 
-    public static PlayerObject INSTANCE = new PlayerObject(Images.get("/assets/characters/PlayerBase.png"), 0, 0, "tmp", new ArrayList<>());
+    public static PlayerObject INSTANCE = new PlayerObject(Images.get("/assets/characters/PlayerBase.png"), 0, 0, new ArrayList<>());
 
-    public PlayerObject() {
-        super(Images.get("/assets/characters/PlayerBase.png"));
-    }
-
-    public PlayerObject(Image sprite, int x, int y, String name, ArrayList<ItemObject> inventar){
+    public PlayerObject(Image sprite, int x, int y, ArrayList<ItemObject> inventar){
         super (sprite, x, y);
-        this.name = name;
         this.inventar = inventar;
     }
 
     //Sobald der Spieler angeklickt wird, wird das Inventar geöffnet
-    /*
     @Override
     public void update(BaseScene scene) {
         if(isClicked())
             SceneStack.INSTANCE.push(new InventarScene());
     }
-     */
     
 }
