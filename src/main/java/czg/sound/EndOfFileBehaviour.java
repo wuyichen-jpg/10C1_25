49d26b8eaeb13104de51162da87385db1ab906f6
helmo2019 <helmo2019@yahoo.com>
2026-03-14 17:29:52 +0100

    EndOfFileBehaviour zu einer Klasse machen und .andThen() hinzufügen

--- src/main/java/czg/sound/EndOfFileBehaviour.java
+++ src/main/java/czg/sound/EndOfFileBehaviour.java
@@ -2,29 +2,43 @@ package czg.sound;
 
 import java.util.function.Consumer;
 
-public enum EndOfFileBehaviour {
+public class EndOfFileBehaviour {
 
     /**
      * Von Anfang an abspielen
      */
-    LOOP(sound -> sound.seek(0)),
+    public static final EndOfFileBehaviour LOOP = new EndOfFileBehaviour(sound -> sound.seek(0));
 
     /**
      * Zurück zum Anfang spulen und pausieren
      */
-    RESTART_AND_PAUSE(sound -> {
+    public static final EndOfFileBehaviour RESTART_AND_PAUSE = new EndOfFileBehaviour(sound -> {
         LOOP.function.accept(sound);
         sound.setPlaying(false);
-    }),
+    });
 
     /**
      * Stoppen
      */
-    STOP(BaseSound::stop);
+    public static final EndOfFileBehaviour STOP = new EndOfFileBehaviour(BaseSound::stop);
 
+    /**
+     * Auszuführende Funktion
+     */
     public final Consumer<BaseSound> function;
 
-    EndOfFileBehaviour(Consumer<BaseSound> function) {
+    /**
+     * Erstellt ein neues {@link EndOfFileBehaviour}-Objekt, welches nach
+     * der {@link #function} dieses Objekts zusätzlich die in dieser Funktion
+     * angegebene ausführt
+     * @param followUpFunction Zusätzlich auszuführende Funktion
+     * @return Ein neues {@link EndOfFileBehaviour}-Objekt
+     */
+    public EndOfFileBehaviour andThen(Consumer<BaseSound> followUpFunction) {
+        return new EndOfFileBehaviour(this.function.andThen(followUpFunction));
+    }
+
+    private EndOfFileBehaviour(Consumer<BaseSound> function) {
         this.function = function;
     }
 
