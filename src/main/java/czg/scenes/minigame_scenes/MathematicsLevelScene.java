881af2e7387f77863b2715f1563d817410189384
KOEZIGSKOBR4 <insertemail@here.com>
2026-03-16 21:26:48 +0100

    hinzufügen des ersten puzzles

--- src/main/java/czg/scenes/minigame_scenes/MathematicsLevelScene.java
+++ src/main/java/czg/scenes/minigame_scenes/MathematicsLevelScene.java
@@ -1,7 +1,10 @@
 package czg.scenes.minigame_scenes;
 
+import czg.MainWindow;
+import czg.objects.BaseObject;
 import czg.objects.Department;
 import czg.objects.minigame_objects.TangramPieceObject;
+import czg.util.Images;
 
 import java.util.Arrays;
 
@@ -11,6 +14,18 @@ public class MathematicsLevelScene extends LevelScene {
 
         objects.addAll(Arrays.asList(TangramPieceObject.PIECES));
 
-        TangramPieceObject.generatePacked(100, 100, 400, 400);
+        int size = MainWindow.HEIGHT / 2;
+        double scale = Images.get("/assets/minigames/mathematics/tangram_packed.png").getWidth(null) / (double) size;
+        TangramPieceObject.generatePacked((MainWindow.HEIGHT - size) / 2, (MainWindow.HEIGHT - size) / 2, size, size);
+
+        BaseObject puzzle = new BaseObject(Images.get("/assets/minigames/mathematics/puzzle_1_1.png"), 0, 0);
+
+        puzzle.width *= scale;
+        puzzle.height *= scale;
+
+        puzzle.x = (int) (MainWindow.WIDTH - puzzle.width - MainWindow.WIDTH*0.1);
+        puzzle.y = (MainWindow.HEIGHT - puzzle.height)/2;
+
+        objects.add(puzzle);
     }
 }
