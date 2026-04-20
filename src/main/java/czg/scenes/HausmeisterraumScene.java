e2d2a2bc8c4245f2e466b082d7805a85e7a2c24a
helmo2019 <helmo2019@yahoo.com>
2026-04-20 18:06:21 +0200

    Peak easteregg

--- src/main/java/czg/scenes/HausmeisterraumScene.java
+++ src/main/java/czg/scenes/HausmeisterraumScene.java
@@ -1,10 +1,10 @@
 
 package czg.scenes;
 
-import czg.objects.BackdropObject;
-import czg.objects.PfeilObject;
-import czg.objects.PlayerObject;
+import czg.objects.*;
 import czg.util.Images;
+
+import static czg.MainWindow.PIXEL_SCALE;
 
 public class HausmeisterraumScene extends BaseScene{
     public HausmeisterraumScene() {
@@ -13,7 +13,17 @@ public class HausmeisterraumScene extends BaseScene{
         
         //Pfeilobjekt für den Wechsel in die Gangszene
         objects.add(new PfeilObject(this, GangHausmeisterScene::new, PfeilObject.UNTEN));
-        
+
+        objects.add(new ButtonObject(null, 105 * PIXEL_SCALE, 64 * PIXEL_SCALE, 16, 16, () -> {
+            if(! PlayerObject.INSTANCE.inventar.contains(ItemType.TEXT))
+                PlayerObject.INSTANCE.inventar.add(ItemType.TEXT);
+        }));
+
+        objects.add(new ButtonObject(null, 31 * PIXEL_SCALE, 52 * PIXEL_SCALE, 12, 12, () -> {
+            if(! PlayerObject.INSTANCE.inventar.contains(ItemType.PAPIER))
+                PlayerObject.INSTANCE.inventar.add(ItemType.PAPIER);
+        }));
+
         //Einfügen der Spieler-Figur
         this.objects.add(PlayerObject.INSTANCE);
         PlayerObject.INSTANCE.x = 230;
